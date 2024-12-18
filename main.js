edd333362592533fa1014c7dbe6923cf9fcb1960
SUBROTOKOIRY <koirysubroto1103@gmail.com>
2024-12-18 15:52:49 +0530

    updated code

--- main.js
+++ main.js
@@ -7,21 +7,32 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active')
 }
 
+/*========================= typed js =========================*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Full Stack Developer', 'Devops Geek', 'Learner'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+})
+
 /*========================= scroll section active link =========================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  sections.forEach(sec => {
+    let top = window.scrollY;
+
+    let offset = sec.offsetTop - 150;
+    // console.log(top , sec.offsetTop,offset);
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
             });
         };
     });
@@ -42,19 +53,12 @@ ScrollReveal({
     delay: 200,
 });
 
-ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'buttom' });
-ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
 
-/*========================= typed js =========================*/
-const typed = new Typed('.multiple-text', {
-    strings: ['Full Stack Developer', 'Devops Geek', 'Learner'],
-    typeSpeed: 70,
-    backSpeed: 70,
-    backDelay: 1000,
-    loop: true,
-})
+ScrollReveal().reveal('.home-content heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'buttom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal(' .about-content .progress-bars', { origin: 'right' });
+
 
 
 /*========================= Contact section =========================*/
